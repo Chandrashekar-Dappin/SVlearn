50f120a3e8cea57afb6e1a83e5a70911539561a5
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-07-17 01:02:29 +0530

    Update mimic of fork-join_any using fork-join_none.v

--- fork join - parallel threading/mimic of fork-join_any using fork-join_none.v
+++ fork join - parallel threading/mimic of fork-join_any using fork-join_none.v
@@ -27,6 +27,33 @@ module tb;
 endmodule
 
 // output
-// thread 1
-// outer thread
-// thread 2
+thread 1
+outer thread
+thread 2
+
+
+// another method : by giving different delays
+module tb;
+  
+  initial begin
+    
+    fork
+      #3 $display("thread A");
+      $display("thread B");
+      #10 $display("thread C");
+    join_none
+      
+    #1 $display("after fork join_none");
+      
+  end
+      
+endmodule
+
+//Output
+thread B
+after fork join_none
+thread A
+thread C
+
+
+
